0a42061656198f337307c3f607274cb0c887d8dd
Rich Youngkin <rich.youngkin@gmail.com>
2014-09-13 16:07:04 -0600

    Playing around with markdown in the README.md file.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 erl-web-zookeeper
 =================
 
-Sample app demonstrating use of Erlang supervision, release with multiple apps, web interface, and quorum election via Zookeeper.
+Sample app demonstrating use of Erlang supervision, release with multiple apps, web interface, 
+and quorum election via Zookeeper.
+
+**This is very early days for this project, it's not quite ready to use for anything.  Just 
+wanted to get it up on github.**
+
+#Prerequisites
+
+[http://zookeeper.apache.org/](Apache Zookeeper) is one of the keystones for this project.
+* At least one zookeeper instance should be available.  The src/ez.app.src file contains the configuration for zookeeper.  The `{hosts {...}}` tuple should be modified to match up with the zookeeper installation.
--- start_in_shell.sh
+++ start_in_shell.sh
@@ -1,1 +1,9 @@
-erl -K true +P 10000 -env ERL_MAX_PORTS 10000 -pa ebin deps/lager/ebin deps/goldrush/ebin deps/ezk/ebin -boot start_sasl -name suptest@rich.suptest.com -setcookie COOKIE -config erl-zookeeper.config -eval "application:start(compiler),application:start(syntax_tools),application:start(goldrush),application:start(lager),application:start(ezk),application:start(ez)."
+erl -K true +P 10000 -env ERL_MAX_PORTS 10000 							\
+	-pa ebin deps/lager/ebin deps/goldrush/ebin deps/ezk/ebin 			\
+	-boot start_sasl 													\
+	-name suptest@rich.suptest.com 										\
+	-setcookie COOKIE 													\
+	-config erl-zookeeper.config 										\
+	-eval "application:start(compiler),application:start(syntax_tools),	\
+	application:start(goldrush),application:start(lager), 				\
+	application:start(ezk),application:start(ez)."
